73b50973d897bfe277ff5d7da0638524612127b4
RicardoAtNova <ricardo@novaapps.cloud>
2022-07-25 09:48:11 +0200

    Paginated List and extensions

---
+++ Ojave.Core/Extensions/Queryable/PaginatedQueryableExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Ojave.Core.Extensions;
+
+public static class PaginatedQueryableExtensions
+{
+    public static IQueryable<T> SkipWithPagination<T>(this IQueryable<T> query, int pageNumber, int qtyPerPage)
+        => query.Skip((pageNumber - 1) * qtyPerPage);
+
+    public static IQueryable<T> SkipWithPagination<T>(this IOrderedQueryable<T> query, int pageNumber, int qtyPerPage)
+        => query.Skip((pageNumber - 1) * qtyPerPage);
+}
