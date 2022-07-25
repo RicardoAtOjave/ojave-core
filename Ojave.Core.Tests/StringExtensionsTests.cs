3e6b3fade88c096d33a60501986845b6c8ad75b0
RicardoAtNova <ricardo@novaapps.cloud>
2022-07-25 10:44:11 +0200

    added extension overloads for determining splitters for string extensions with tests

---
+++ Ojave.Core.Tests/StringExtensionsTests.cs
@@ -0,0 +1,50 @@
+﻿using Ojave.Core.Extensions;
+
+namespace Ojave.Core.Tests;
+
+public class StringExtensionsTests
+{
+    private string lettersAndNumbers = "CE10090-F01-V02";
+    private string lettersAndNumbersSemiColon = "CE10090;F01;V02";
+    private string expected = "10090";
+
+    [Fact]
+    public void OnlyNumberExtTest()
+    {
+        var noLetters = lettersAndNumbers.OnlyNumbers();
+        Assert.Equal("100900102", noLetters);
+    }
+
+    [Fact]
+    public void OnlyNumberExtSplitterTest()
+    {
+        var noLetters = lettersAndNumbers.OnlyNumbers('-');
+        var noLetters2 = lettersAndNumbers.OnlyNumbers('/');
+        var noLetters3 = lettersAndNumbersSemiColon.OnlyNumbers(';');
+
+        Assert.Equal(expected, noLetters);
+        Assert.Equal(string.Empty, noLetters2);
+        Assert.Equal(expected, noLetters3);
+    }
+
+    [Fact]
+    public void OnlyNumbersExtSkipAndSplitterTest()
+    {
+        var noLetters1 = lettersAndNumbers.OnlyNumbers(0, '-');
+        var noLetters2 = lettersAndNumbers.OnlyNumbers(1, '-');
+        var noLettersNoSplitter = lettersAndNumbers.OnlyNumbers(1, '/');
+        var noLettersSemiCol = lettersAndNumbers.OnlyNumbers(1, ';');
+
+        Assert.Equal(expected, noLetters1);
+        Assert.Equal("01", noLetters2);
+        Assert.Equal(string.Empty, noLettersNoSplitter);
+        Assert.Equal(string.Empty, noLettersSemiCol);
+    }
+
+    [Fact]
+    public void OnlyLettersExtTest()
+    {
+        var noNumbers = lettersAndNumbers.OnlyLetters();
+        Assert.Equal("CEFV", noNumbers);
+    }
+}
--- Ojave.Core/Extensions/StringExtensions.cs
+++ Ojave.Core/Extensions/StringExtensions.cs
@@ -1,9 +1,86 @@
-﻿namespace Ojave.Core.Extensions;
+﻿using Ojave.Core.System;
+
+namespace Ojave.Core.Extensions;
 
 public static class StringExtensions
 {
-    public static string OnlyNumbers(this ReadOnlySpan<string> span)
+    /// <summary>
+    /// Will return all numbers found within a string, removing all other characters
+    /// </summary>
+    /// <param name="str"></param>
+    /// <returns>string of numbers</returns>
+    public static string OnlyNumbers(this string str)
     {
+        char[] numbers = Character.RemoveLetters(str);
+        return new string(numbers);
+    }
 
+    /// <summary>
+    /// Will return all numbers before the first instance of the splitter provided is found, removing all other characters
+    /// </summary>
+    /// <param name="str"></param>
+    /// <param name="splitter"></param>
+    /// <returns>string of numbers</returns>
+    public static string OnlyNumbers(this string str, char splitter)
+    {
+        var indexed = str.IndexOf(splitter);
+
+        if (indexed < 0 || indexed > str.Length) return string.Empty;
+
+        var indexedSpan = str[..indexed];
+
+        char[] numbers = Character.RemoveLetters(indexedSpan);
+
+        return new string(numbers);
+    }
+
+    /// <summary>
+    /// Will return all numbers from a string removing all other characters while skipping the number of instances of a splitter determined by the skip parameter provided. 
+    /// </summary>
+    /// <param name="str"></param>
+    /// <param name="skip"></param>
+    /// <param name="splitter"></param>
+    /// <returns>string of numbers</returns>
+    public static string OnlyNumbers(this string str, int skip, char splitter)
+    {
+        int splitterIndex = 0;
+        int startIndex = 0;
+        int endIndex = 0;
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (str[i] == splitter)
+            {
+                splitterIndex++;
+
+                if (splitterIndex == skip)
+                {
+                    startIndex = i + 1;
+                }
+
+                if (splitterIndex > skip)
+                {
+                    endIndex = i;
+                    break;
+                }
+            }
+        }
+
+        var selection = str[startIndex..endIndex];
+
+        char[] numbers = Character.RemoveLetters(selection);
+
+        return new string(numbers);
+    }
+
+    /// <summary>
+    /// Will return all letters found within a string, removing all other characters
+    /// </summary>
+    /// <param name="str"></param>
+    /// <returns>string of letters</returns>
+    public static string OnlyLetters(this string str)
+    {
+        char[] letters = Character.RemoveNumbers(str);
+        return new string(letters);
     }
 }
