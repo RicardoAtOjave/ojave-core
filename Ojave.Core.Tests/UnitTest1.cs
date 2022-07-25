30abbb00d8fb05149b42e1d643304449fb52955c
RicardoAtNova <ricardo@novaapps.cloud>
2022-07-25 09:39:25 +0200

    Add project files.

---
+++ Ojave.Core.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Ojave.Core.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Ojave.Core/Extensions/Iterable/IterableStringExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Ojave.Core.Extensions;
+
+public static class IterableStringExtensions
+{
+    public static IEnumerable<string> ToLower(this IEnumerable<string> collection)
+    {
+        var arr = collection.ToArray();
+        string[] newCollection = new string[arr.Length];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            newCollection[i] = arr[i].ToLower();
+        }
+
+        return newCollection;
+    }
+}
