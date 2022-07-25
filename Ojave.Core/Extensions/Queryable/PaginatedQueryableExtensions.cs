eaf6cf20c820cf106ed75de3eee12b2a52be06b6
RicardoAtNova <ricardo@novaapps.cloud>
2022-07-25 13:35:59 +0200

    added .ToPaginatedListAsync

--- Ojave.Core/Extensions/Queryable/PaginatedQueryableExtensions.cs
+++ Ojave.Core/Extensions/Queryable/PaginatedQueryableExtensions.cs
@@ -1,7 +1,12 @@
-﻿namespace Ojave.Core.Extensions;
+﻿using Ojave.Core.System.Collections;
+
+namespace Ojave.Core.Extensions;
 
 public static class PaginatedQueryableExtensions
 {
+    public static Task<PaginatedList<TDestination>> ToPaginatedListAsync<TDestination>(this IQueryable<TDestination> queryable, int pageNumber, int qtyPerPage)
+           => PaginatedList<TDestination>.CreateAsync(queryable, pageNumber, qtyPerPage);
+
     public static IQueryable<T> SkipWithPagination<T>(this IQueryable<T> query, int pageNumber, int qtyPerPage)
         => query.Skip((pageNumber - 1) * qtyPerPage);
 
