a37c7d18539540ada7919ca330dc5e00aad98646
RicardoAtNova <ricardo@novaapps.cloud>
2022-09-12 15:19:07 +0200

    created record and packed and shipped

---
+++ Ojave.Core/Http/CreatedRecordId.cs
@@ -0,0 +1,11 @@
+﻿namespace Ojave.Core.Http;
+
+/// <summary>
+/// This is to be used in the serialized response of the HttpCoreResponse to note the new record id inserted into the database
+/// as opposed to simply returning an integer, string or Guid
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class CreatedRecordResponse<T>
+{
+    public T CreatedRecordId { get; set; }
+}
