ae26867aadaff185b88c9ddc6a76a9c761537aa9
RicardoAtNova <ricardo@novaapps.cloud>
2022-09-12 14:24:02 +0200

    added httpcoreresponse

---
+++ Ojave.Core/Http/HttpCoreResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace Ojave.Core.Http;
+
+public sealed class HttpCoreResponse<T>
+{
+    public HttpCoreResponse(int statusCode, string message, T data)
+    {
+        StatusCode = statusCode;
+        Message = message;
+        Data = data;
+    }
+
+    public int StatusCode { get; set; }
+    public string Message { get; set; }
+    public T Data { get; set; }
+}
