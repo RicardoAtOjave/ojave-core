62c7feef641803e4c6452a645a03f8b25036358a
RicardoAtNova <ricardo@novaapps.cloud>
2022-09-20 20:48:54 +0200

    added combne collection for list of integers

--- Ojave.Core/Extensions/Iterable/IterableExtensions.cs
+++ Ojave.Core/Extensions/Iterable/IterableExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 
 namespace Ojave.Core.Extensions.Iterable;
 
@@ -31,6 +32,48 @@ public static class IterableExtensions
         builder.Append(collection[0]);
 
         var span = collection.AsSpan()[1..];
+
+        foreach (var item in span)
+        {
+            builder.Append($"{separator} {item}");
+        }
+
+        return builder.ToString();
+    }
+
+    public static string CombineCollection(this List<int> collection)
+    {
+        StringBuilder builder = new();
+
+        if (collection.Count == 0) return string.Empty;
+
+        builder.Append(collection[0]);
+
+        var span = collection
+            .Skip(1)
+            .ToArray()
+        .AsSpan();
+
+        foreach (var item in span)
+        {
+            builder.Append($" {item}");
+        }
+
+        return builder.ToString();
+    }
+
+    public static string CombineCollection(this List<int> collection, char separator)
+    {
+        StringBuilder builder = new();
+
+        if (collection.Count == 0) return string.Empty;
+
+        builder.Append(collection[0]);
+
+        var span = collection
+            .Skip(1)
+            .ToArray()
+            .AsSpan();
 
         foreach (var item in span)
         {
