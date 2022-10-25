555b0958b55ec0b9059768418a84ed6445ef49e1
RicardoAtNova <ricardo@novaapps.cloud>
2022-10-25 12:59:09 +0200

    date time extensions

---
+++ Ojave.Core/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Ojave.Core.System;
+
+namespace Ojave.Core.Extensions;
+public static class DateTimeExtensions
+{
+    public static (DateTime, DateTime) BetweenPeriod(this DateTime date, TimePeriod timePeriod = TimePeriod.Day)
+    {
+        switch (timePeriod)
+        {
+            case TimePeriod.Day:
+                return (date.Date, date.Date.AddDays(1).AddTicks(-1));
+            case TimePeriod.Week:
+                int diff = (7 + (date.DayOfWeek - DayOfWeek.Monday)) % 7;
+                var mondayDate = date.AddDays(-1 * diff).Date;
+                var sundayDate = mondayDate.AddDays(7);
+                return (mondayDate, sundayDate);
+            case TimePeriod.Month:
+                var firstDayOfMonth = new DateTime(date.Year, date.Month, 1);
+                var lasyDayOfMonth = firstDayOfMonth.AddMonths(1).AddTicks(-1);
+                return (firstDayOfMonth, lasyDayOfMonth);
+            case TimePeriod.Year:
+                var yearStart = new DateTime(date.Year, 1, 1);
+                var yearEnd = new DateTime(date.Year, 12, 31);
+                return (yearStart, yearEnd);
+            default:
+                return (date, date);
+        }
+    }
+}
---
+++ Ojave.Core/System/Enums.cs
@@ -0,0 +1,9 @@
+﻿namespace Ojave.Core.System;
+
+public enum TimePeriod
+{
+    Day,
+    Week,
+    Month,
+    Year,
+}
