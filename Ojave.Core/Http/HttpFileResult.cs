43a88987c5a60786eb4c8ca2fefee9729315690d
RicardoAtNova <ricardo@novaapps.cloud>
2022-10-25 17:40:53 +0200

    file result added

---
+++ Ojave.Core/Http/HttpFileResult.cs
@@ -0,0 +1,22 @@
+﻿namespace Ojave.Core.Http;
+
+public sealed class HttpFileResult
+{
+    public HttpFileResult(byte[] bytes, string contentType)
+    {
+        FileContents = bytes;
+        ContentType = contentType;
+        FileDownloadName = string.Empty;
+    }
+
+    public HttpFileResult(byte[] bytes, string contentType, string downloadName)
+    {
+        FileContents = bytes;
+        ContentType = contentType;
+        FileDownloadName = downloadName;
+    }
+
+    public byte[] FileContents { get; private init; }
+    public string ContentType { get; private init; }
+    public string FileDownloadName { get; private init; }
+}
