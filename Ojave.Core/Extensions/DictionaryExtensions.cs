a9bae1cb53bb43dbffe12d4f3ea80770dfba1873
RicardoAtNova <ricardo@novaapps.cloud>
2022-12-30 23:12:05 +0200

    update

---
+++ Ojave.Core/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Ojave.Core.Extensions;
+
+public static class DictionaryExtensions
+{
+    public static int GetValueOrDefault(this Dictionary<int, int> dict, int key)
+    {
+        return dict.ContainsKey(key) 
+               ? dict[key] 
+               : 0;
+    }
+}
