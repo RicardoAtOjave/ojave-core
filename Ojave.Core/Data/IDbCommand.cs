52b906047a6e6ea0ef8eb6b01ef44d8c344515ed
RicardoAtNova <ricardo@novaapps.cloud>
2024-01-19 09:08:21 +0200

    added data section

---
+++ Ojave.Core/Data/IDbCommand.cs
@@ -0,0 +1,8 @@
+﻿using Ojave.Core.System;
+
+namespace Ojave.Core.Data;
+
+public interface IDbCommand<TInput, TResult> where TResult : Result<TResult>
+{
+    Task<Result<TResult>> ExecuteAsync(TInput input, CancellationToken cancellationToken);
+}
---
+++ Ojave.Core/Data/IDbQuery.cs
@@ -0,0 +1,13 @@
+﻿using Ojave.Core.System;
+
+namespace Ojave.Core.Data;
+
+public interface IDbQuery<TResult>
+{
+    Task<Result<TResult>> GetAsync(CancellationToken cancellationToken);
+}
+
+public interface IDbQuery<TInput, TResult>
+{
+    Task<Result<TResult>> GetAsync(TInput input, CancellationToken cancellationToken);
+}
