28afc2597ba4005ca2619353bc0e1f0e0a48eed6
RicardoAtNova <ricardo@novaapps.cloud>
2024-01-29 20:57:20 +0200

    updates

--- Ojave.Core/System/PagedBase.cs
+++ Ojave.Core/System/PagedBase.cs
@@ -2,6 +2,14 @@
 
 public class PagedBase
 {
+    public PagedBase() {}
+
+    public PagedBase(int pageNumber, int qtyPerPage)
+    {
+        PageNumber = pageNumber;
+        QtyPerPage = qtyPerPage;
+    }
+
     public int PageNumber { get; set; } = 1;
     public int QtyPerPage { get; set; } = 30;
 }
